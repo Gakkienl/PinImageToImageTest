811ecdb90edd7f105e2e407196aabebe57d44337
developer@gakkie.nl <developer@gakkie.nl>
2023-06-08 12:04:12 +0200

    Initial Commit

---
+++ PinImageToImageTest/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  PinImageToImageTest
+//
+//  Created by Gakkie Gakkienl on 03/06/2023.
+//
+
+import SwiftUI
+
+let arrowPointUp = Image(systemName: "arrowtriangle.up.fill")
+
+struct ContentView: View {
+    @State private var tapLocation = CGPoint.zero
+
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                Image("worldMap")
+                    .resizable()
+                
+                arrowPointUp
+                    .foregroundColor(.green)
+                    .position(tapLocation)
+                
+                arrowPointUp
+                    .foregroundColor(.blue)
+                    .position(x: 670, y: 389)
+
+                arrowPointUp
+                    .foregroundColor(.blue)
+                    .position(x: 1246, y: 467)
+            }
+            .coordinateSpace(name: "mapImage")
+            .frame(width: proxy.size.width, height: proxy.size.height)
+            .scaledToFit()
+            .clipShape(Rectangle())
+            .PinchToZoomAndPan(contentSize: CGSize(width: proxy.size.width, height: proxy.size.height), tapLocation: $tapLocation)
+//            .onTapGesture { location in
+//                print("Tapped at \(location)")
+//                tapLocation = location
+//            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinImageToImageTest/PinImageToImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinImageToImageTestApp.swift
+//  PinImageToImageTest
+//
+//  Created by Gakkie Gakkienl on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinImageToImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
